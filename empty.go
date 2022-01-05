1cf89f8d47add3caa486d318a37d5b826ed6c506
Toine Eetgerink <antonius.eetgerink@ing.com>
2022-01-06 00:14:51 +0100

    inital stuff

---
+++ empty.go
@@ -0,0 +1,4 @@
+package nsxt_golang_library
+
+// This file is here to allow go get -u to be used in the root of the package.
+// Don't touch this! >:)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/teetgerink/nsxt-golang-library.git
+
+go 1.17
